43291d254470a727b624df4c3c35d94db4d93d1b
Josh Griffiths <josh@hakuna.co.uk>
2024-09-04 14:22:15 +0100

    simple authentication

--- apps/daily_news.py
+++ apps/daily_news.py
@@ -5,50 +5,51 @@ import yaml
 from yaml import SafeLoader
 
 import streamlit as st
-import streamlit_authenticator as stauth
 
 from aim.radio.daily_news import DailyNews
 from aim.radio.voice import VoiceGenerator
 
 logger = logging.getLogger(__name__)
 
-# Load User Passwords
-with open('./config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+# Set valid speakers
+VALID_SPEAKERS = ['aim_christie', 'aim_jodie', 'aim_fiona']
 
-# Login page
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
-)
-
-# Creating a login widget
+# Load Secrets
 try:
-    authenticator.login()
-except Exception as e:
-    st.error(e)
-
-if st.session_state.get('authentication_stats', False):
-
-    # Load Secrets
     try:
-        try:
-            ELEVENLABS_API_KEY = st.secrets["ELEVENLABS_API_KEY"]
-            logger.debug("Secrets loaded from Streamlit Secrets")
-        except Exception as e:
-            from dotenv import load_dotenv, find_dotenv
-            load_dotenv(find_dotenv())
-            ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
-            logger.debug("Secrets loaded from .env file")
+        from dotenv import load_dotenv, find_dotenv
+        load_dotenv(find_dotenv())
+        ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+        STREAMLIT_USER = os.getenv("STREAMLIT_USER")
+        STREAMLIT_PASSWORD = os.getenv("STREAMLIT_PASSWORD")
+        logger.debug("Secrets loaded from .env file")
     except Exception as e:
-        logger.error("Failed to load secrets")
-        logger.error(e)
+        ELEVENLABS_API_KEY = st.secrets["ELEVENLABS_API_KEY"]
+        STREAMLIT_USER = st.secrets["STREAMLIT_USER"]
+        STREAMLIT_PASSWORD = st.secrets["STREAMLIT_PASSWORD"]
+        logger.debug("Secrets loaded from Streamlit Secrets")
+except Exception as e:
+    logger.error("Failed to load secrets")
+    logger.error(e)
 
-    VALID_SPEAKERS = ['aim_christie', 'aim_jodie', 'aim_fiona']
+# Check if the user is logged in
+if "logged_in" not in st.session_state:
+    st.session_state['logged_in'] = False
 
+# Login form
+if st.session_state['logged_in'] is False:
+    st.title("Daily News Podcast")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if username == STREAMLIT_USER and password == STREAMLIT_PASSWORD:
+            st.session_state['logged_in'] = True
+            st.success("Logged in.")
+            st.rerun()
+        else:
+            st.error("Invalid username or password")
+
+else:
     # Function to generate the script
     def generate_script(speaker: str):
         async def _generate_script(speaker: str):
